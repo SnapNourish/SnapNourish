06b68f480046d28abe4764101d63f34866fa7692
chhaevely <cpark275@wisc.edu>
2024-11-05 16:56:12 -0600

    make login page, sign up page

--- app/src/main/java/com/cs407/snapnourish/Home.kt
+++ app/src/main/java/com/cs407/snapnourish/Home.kt
@@ -8,28 +8,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.home)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        //Move to Home.kt Page
-        findViewById<Button>(R.id.loginButton).setOnClickListener {
-            val intent = Intent(this, Home::class.java)
-            startActivity(intent)
-        }
-
-        // Move to SignUp.kt Page
-        findViewById<Button>(R.id.signupButton).setOnClickListener {
-            val intent = Intent(this, SignUp::class.java)
-            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/cs407/snapnourish/SignUp.kt
+++ app/src/main/java/com/cs407/snapnourish/SignUp.kt
@@ -8,27 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.signup)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         //Move to Home.kt Page
-        findViewById<Button>(R.id.loginButton).setOnClickListener {
+        findViewById<Button>(R.id.doneButton).setOnClickListener {
             val intent = Intent(this, Home::class.java)
-            startActivity(intent)
-        }
-
-        // Move to SignUp.kt Page
-        findViewById<Button>(R.id.signupButton).setOnClickListener {
-            val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
     }
