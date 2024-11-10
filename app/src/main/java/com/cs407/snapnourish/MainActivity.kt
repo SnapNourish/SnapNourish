dae6e102daa7fa810546c02bb23fd17f3ad2f6bb
Gurleen Hundal <gkhundal1@gmail.com>
2024-11-10 12:57:02 -0600

    Created values for user email and password to check

--- app/src/main/java/com/cs407/snapnourish/MainActivity.kt
+++ app/src/main/java/com/cs407/snapnourish/MainActivity.kt
@@ -16,10 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //    insets
+        //}
+        //var auth = FirebaseAuth.getInstance()
+    }
+    var auth = FirebaseAuth.getInstance()
+    fun login(view: View) {
+        val userEmailIn = findViewById<EditText>(R.id.emailInput)
+        val userEmail = userEmailIn.text.toString()
+        val userPasswordIn = findViewById<EditText>(R.id.passwordInput)
+        val userPassword = userPasswordIn.text.toString()
+        auth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                //val intent = Intent(this)
+            }
         }
     }
+
 }
