13bcf168b4eabc0c54a2a364599e4c3f8d2d61f8
Gurleen Hundal <gkhundal1@gmail.com>
2024-11-19 22:57:29 -0600

    FIgured out that nothing was happening because the login_page_gurleen.xml had ".MainActivity" for the tools:context and not ".login_page_gurleen" which has the authentication code

--- app/src/main/java/com/cs407/snapnourish/LoginPageGurleen.kt
+++ app/src/main/java/com/cs407/snapnourish/LoginPageGurleen.kt
@@ -1,7 +1,9 @@
 package com.cs407.snapnourish
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
@@ -38,18 +40,32 @@ class LoginPageGurleen : AppCompatActivity() {
      * user will be sent to the main menu screen. On failure, a message will appear.
      */
     fun login(view: View) {
-        val userEmailIn = findViewById<EditText>(R.id.emailInput)
-        val userEmail = userEmailIn.text.toString()
+        //val userEmailIn = findViewById<EditText>(R.id.emailInput)
+        //val userEmail = userEmailIn.text.toString()
         // THIS SHOULD WORK! Way to get email and password is correct. Should not get any errors.
         // Format is same as GurleenSnapNourish project I have on GitHub.
-        val userPasswordIn = findViewById<EditText>(R.id.passwordInput)
-        val userPassword = userPasswordIn.text.toString()
-        auth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(this) { task ->
-            if(task.isSuccessful) {
-                //TODO: Go to main screen on successful login
-                val intent= Intent(this,Home::class.java)
+        //val userPasswordIn = findViewById<EditText>(R.id.passwordInput)
+        //val userPassword = userPasswordIn.text.toString()
+        //auth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(this) { task ->
+        //    if(task.isSuccessful) {
+        //        //TODO: Go to main screen on successful login
+        //        val intent= Intent(this,Home::class.java)
+        //        startActivity(intent)
+        //        finish()
+        //    }
+        //}.addOnFailureListener { exception ->
+        //    Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+        //}
+        val userEmail = findViewById<EditText>(R.id.emailInput)
+        val emailUsername = userEmail.text.toString()
+        val userPass = findViewById<EditText>(R.id.passwordInput)
+        val passPass = userPass.text.toString()
+        auth.signInWithEmailAndPassword(emailUsername,passPass).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d(TAG, "signinwithemailsuccess")
+                val user = auth.currentUser
+                val intent = Intent(this,Home::class.java)
                 startActivity(intent)
-                finish()
             }
         }.addOnFailureListener { exception ->
             Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
