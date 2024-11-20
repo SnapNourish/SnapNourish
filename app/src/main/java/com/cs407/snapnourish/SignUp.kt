20fa8ed27e0ab9047e72e137583709152868b8a9
Gurleen Hundal <gkhundal1@gmail.com>
2024-11-19 23:11:53 -0600

    Made context of signup.xml be the SignUp activity and added register function to create new user

--- app/src/main/java/com/cs407/snapnourish/SignUp.kt
+++ app/src/main/java/com/cs407/snapnourish/SignUp.kt
@@ -7,6 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,16 +18,31 @@ class SignUp : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.signup)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup)) { v, insets ->
+        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //    insets
+        //}
 
         //Move to Home.kt Page
-        findViewById<Button>(R.id.doneButton).setOnClickListener {
-            val intent = Intent(this, Home::class.java)
-            startActivity(intent)
+        //findViewById<Button>(R.id.doneButton).setOnClickListener {
+        //    val intent = Intent(this, Home::class.java)
+        //    startActivity(intent)
+        //}
+    }
+    var auth = FirebaseAuth.getInstance()
+    fun register(view: View) {
+        val userEmail = findViewById<EditText>(R.id.emailInput)
+        val email = userEmail.text.toString()
+        val userPassword = findViewById<EditText>(R.id.passwordInput)
+        val password = userPassword.text.toString()
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                val intent = Intent(this,Home::class.java)
+                startActivity(intent)
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
         }
     }
 }
