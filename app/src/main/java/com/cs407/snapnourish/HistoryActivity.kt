e7a90003a34db3b2e671dd54cd283921f28f079b
hyewonp0724@gmail.com <hyewonp0724@gmail.com>
2024-12-11 07:08:57 +0900

    logout style change

--- app/src/main/java/com/cs407/snapnourish/HistoryActivity.kt
+++ app/src/main/java/com/cs407/snapnourish/HistoryActivity.kt
@@ -101,7 +101,10 @@ class HistoryActivity : AppCompatActivity() {
             listOf(
                 Photo(R.drawable.food1, "2024-12-1"),
                 Photo(R.drawable.food2, "2024-12-2"),
-                Photo(R.drawable.food3, "2024-12-3")
+                Photo(R.drawable.food3, "2024-12-3"),
+                Photo(R.drawable.food4, "2024-12-4"),
+                Photo(R.drawable.food5, "2024-12-5")
+
             )
         } else {
             emptyList()
