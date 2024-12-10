8f382fe019abefcfa7c368441d270e01f382c8c6
Gurleen Hundal <gkhundal1@gmail.com>
2024-12-10 15:54:01 -0600

    Added logout button and edited text for login and signup screens

--- app/src/main/java/com/cs407/snapnourish/SettingsActivity.kt
+++ app/src/main/java/com/cs407/snapnourish/SettingsActivity.kt
@@ -3,12 +3,17 @@ package com.cs407.snapnourish
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +24,8 @@ class SettingsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
 
         // Navigate to Home screen(icon1)
         findViewById<Button>(R.id.btn_home).setOnClickListener {
@@ -43,5 +50,24 @@ class SettingsActivity : AppCompatActivity() {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
+
+        auth = FirebaseAuth.getInstance()
+
+        //val signOutButton = findViewById<Button>(R.id.logoutButton)
+        //signOutButton.setOnClickListener {
+        //    signOutUser()
+        //}
+        findViewById<Button>(R.id.logoutButton).setOnClickListener {
+            signOutUser()
+            val intent = Intent(this, LoginPageGurleen::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signOutUser() {
+        auth.signOut()
+        Toast.makeText(this, "Sign out successful", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, LoginPageGurleen::class.java))
+        finish()
     }
 }
